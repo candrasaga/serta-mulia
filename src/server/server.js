0c1179b25ddddc73ff0686eaa0ddcd5e12c23df3
candrasaga <candra@DESKTOP-GFKHTKT>
2024-05-21 19:08:27 +0700

    Push code to remote repository

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
@@ -23,14 +24,14 @@ const loadModel = require('../services/loadModel');
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
  
-        // if (response instanceof InputError) {
-        //     const newResponse = h.response({
-        //         status: 'fail',
-        //         message: `${response.message} Silakan gunakan foto lain.`
-        //     })
-        //     newResponse.code(response.statusCode)
-        //     return newResponse;
-        // }
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message} Silakan gunakan foto lain.`
+            })
+            newResponse.code(response.statusCode)
+            return newResponse;
+        }
  
         if (response.isBoom) {
             const newResponse = h.response({
